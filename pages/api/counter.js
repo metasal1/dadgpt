562d9cbc3de62825d8adac5052b0b69de1a09c9b
Salim Karim <imsjk8@gmail.com>
2023-08-23 11:37:03 +1000

    counter

--- pages/api/counter.js
+++ pages/api/counter.js
@@ -5,28 +5,22 @@ export default async function handler(req, res) {
   const dbName = 'dadgpt'; // Replace with your specific database name
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-  if (ip === '127.0.0.1' || ip === '117.102.147.149') {
-
-    if (req.method !== 'GET') {
-      return res.status(405).json({ error: 'Method Not Allowed' });
-    }
-
-    try {
-      const client = new MongoClient(mongoURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      await client.connect();
-      const db = client.db(dbName);
-      const count = await db.collection('queries').countDocuments();
-      res.status(200).json({ count });
-      client.close();
-    } catch (error) {
-      console.error('Error fetching document count:', error);
-      res.status(500).json({ error: 'Error fetching document count' });
-    }
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method Not Allowed' });
   }
-  else {
-    res.status(403).json({ error: 'Forbidden' });
+
+  try {
+    const client = new MongoClient(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    const db = client.db(dbName);
+    const count = await db.collection('queries').countDocuments();
+    res.status(200).json({ count });
+    client.close();
+  } catch (error) {
+    console.error('Error fetching document count:', error);
+    res.status(500).json({ error: 'Error fetching document count' });
   }
 }
