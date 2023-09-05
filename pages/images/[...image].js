bd1feba8927ee235919fd546fc37f813462bd3eb
Salim Karim <imsjk8@gmail.com>
2023-09-05 22:10:30 +1000

    added images route

--- pages/images/[...image].js
+++ pages/images/[...image].js
@@ -1,9 +1,25 @@
 import { useRouter } from 'next/router'
 import Image from 'next/image'
+import Head from 'next/head'
 
 export default function Page() {
     const router = useRouter()
     return <>
-        <Image src={`https://i.imgur.com/${router.query.image}.png`} width={400} height={600} />
+        <Head>
+            <title>DadGippity by Salim Karim</title>
+            <meta name="description" content="When Dads not around to answer your questions" />
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:site" content="@dadgippity" />
+            <meta name="twitter:creator" content="@metasal_" />
+            <meta property="twitter:title" content="Come talk to DadGippity" />
+            <meta property="twitter:image" content={`https://i.imgur.com/${router.query.image}.png`} />
+            <meta property="og:image" content={`https://i.imgur.com/${router.query.image}.png`} />
+            <meta property="twitter:description" content="When dad is not around to answer questions you have" />
+            <meta property="og:url" content="https://dadgippity.com" />
+            <meta property="og:title" content="Come talk to DadGippity" />
+            <meta property="og:description" content="When dad is not around to answer questions you have" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Image src={`https://i.imgur.com/${router.query.image}.png`} width={500} height={800} />
     </>
 }
